962b205218b06e6eae8bfeec17a494742cad482d
manelelakel <manel.elakel@gmail.com>
2025-10-05 21:08:16 +0200

    Ajout du fichier app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.title(" Suivie des Depenses ")
+df = pd.read_csv("sample_data.csv")
+st.subheader("Liste des depenses")
+st.dataframe(df)
+st.subheader("depenses par categorie")
+fig2,ax2=plt.subplots()
+df.groupby("Category")["Amount"].sum().plot(kind="bar" ,ax=ax)
+st.pyplot(fig)
+st.subheader("Depenses par date")
+fig2,ax2=plt.subplots()
+df.groupby("Date")["Amount"].sum().plot(kind="line ,marker="0" ,ax=ax2)
+st.pyplot(fig2)
